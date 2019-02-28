2f948ccce72b453c765ec06b111db047918b67d6
Gustav Westberg <gustavwes@gmail.com>
2019-02-28 13:25:01 +0100

    Setup of bare-bone member funcitonality (until we decide authentication etc.)

--- TpixAPI/Startup.cs
+++ TpixAPI/Startup.cs
@@ -43,6 +43,7 @@ namespace TpixAPI
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<ITopicRepository, TopicRepository>();
             services.AddScoped<IPostRepository, PostRepository>();
+            services.AddScoped<IMemberRepository, MemberRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
